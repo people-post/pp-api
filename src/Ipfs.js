a03276ba55ef6f5f5ece10df894c67a29bdc7b3c
hongwei <yhw1630@gmail.com>
2025-10-30 15:09:18 -0700

    Bug fix

--- src/Ipfs.js
+++ src/Ipfs.js
@@ -1,16 +1,20 @@
-import {verifiedFetch} from '@helia/verified-fetch';
+import {createVerifiedFetch} from '@helia/verified-fetch';
 import {CID} from 'multiformats';
 
 export default class Ipfs {
+  #vFetch;
+
+  async asSetHelia(helia) { this.#vFetch = await createVerifiedFetch(helia); }
+
   async asFetchCidJson(scid) {
     const cid = CID.parse(scid);
-    const r = await verifiedFetch(cid, {signal : AbortSignal.timeout(20000)});
+    const r = await this.#vFetch(cid, {signal : AbortSignal.timeout(20000)});
     return await r.json()
   }
 
   async asFetchCidImage(scid) {
     const cid = CID.parse(scid);
-    const r = await verifiedFetch(cid, {signal : AbortSignal.timeout(20000)});
+    const r = await this.#vFetch(cid, {signal : AbortSignal.timeout(20000)});
     let blob = await r.blob();
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
