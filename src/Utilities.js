a3f5ea10737ffef256d9b202d8094ec5330dafc0
hongwei <yhw1630@gmail.com>
2025-11-11 14:44:05 -0800

    Added more utl

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,3 +1,5 @@
+import {keys} from '@libp2p/crypto';
+import {peerIdFromPublicKey, peerIdFromString} from '@libp2p/peer-id';
 import {CID} from 'multiformats';
 
 export default class Utilities {
@@ -11,5 +13,12 @@ export default class Utilities {
     } catch (e) {
       return false;
     }
+  }
+
+  peerIdFromString(s) { return peerIdFromString(s); }
+  peerIdFromPublicKey(k) { return peerIdFromPublicKey(k); }
+
+  async asGenerateKeyPairFromSeed(type, seed) {
+    return await keys.generateKeyPairFromSeed(type, seed);
   }
 };
