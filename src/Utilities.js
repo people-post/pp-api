216ee3be5da6c7043baa53215c031dcb21e8a0f6
hongwei <yhw1630@gmail.com>
2025-11-19 09:09:29 -0800

    Added mlDsa44 key

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,5 +1,6 @@
-import {keys} from '@libp2p/crypto';
+import {keys as Libp2pKeys} from '@libp2p/crypto';
 import {peerIdFromPublicKey, peerIdFromString} from '@libp2p/peer-id';
+import {ml_dsa44 as MlDsa44} from '@noble/post-quantum/ml-dsa.js';
 import {CID} from 'multiformats';
 
 export default class Utilities {
@@ -18,7 +19,13 @@ export default class Utilities {
   peerIdFromString(s) { return peerIdFromString(s); }
   peerIdFromPublicKey(k) { return peerIdFromPublicKey(k); }
 
-  async asGenerateKeyPairFromSeed(type, seed) {
-    return await keys.generateKeyPairFromSeed(type, seed);
+  async asEd25519KeyGen(seed) {
+    return await Libp2pKeys.generateKeyPairFromSeed("Ed25519", seed);
+  }
+
+  mlDsa44KeyGen(seed) { return MlDsa44.keygen(seed); }
+  mlDsa44Sign(msg, key) {
+    // msg is Uint8Array, key is secretKey of MlDsa44 key pair
+    return MlDsa44.sign(msg, key);
   }
 };
